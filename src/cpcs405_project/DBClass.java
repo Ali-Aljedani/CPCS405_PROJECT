fd0010ab09e61650ec4839393ac5808e3a92eb8a
Ali-Aljedani <110301409+Ali-Aljedani@users.noreply.github.com>
2023-02-11 17:33:14 +0300

    Add files via upload

---
+++ src/cpcs405_project/DBClass.java
@@ -0,0 +1,207 @@
+package cpcs405_project;
+
+
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.time.LocalDate;
+
+
+public class DBClass {
+
+    private static int ID;
+    private static int RecordID = 0;
+    private final String url;
+    private final String dbusername;
+    private final String dbpassword;
+    private Connection con ;
+
+    public DBClass() throws SQLException {
+        this.url = "jdbc:derby://localhost:1527/GroupProject405";
+        this.dbusername = "sa";
+        this.dbpassword = "sa";
+        this.con = DriverManager.getConnection(url, dbusername, dbpassword);
+    }
+
+    public void getUsersTable() {
+
+        String query = "SELECT * From USERS";
+
+        try {
+            Statement stet = con.createStatement();
+            ResultSet rs = stet.executeQuery(query);
+            //Add date
+            while (rs.next()) {
+                DBClass.ID++;
+                int UID = rs.getInt("UserID");
+                String username = rs.getString("username");
+                String password = rs.getString("password");
+                String firstName = rs.getString("firstName");
+                String lastName = rs.getString("lastName");
+                Date date = rs.getDate("date");
+                System.out.println("User ID: " + UID
+                        + "\nUsername: " + username
+                        + "\nPassword: " + password
+                        + "\nFirst Name: " + firstName
+                        + "\nLast Name: " + lastName
+                        + "\nDate: " + date);
+            }
+        } catch (SQLException e) {
+            System.out.println("SQL Excption " + e);
+        }
+    }
+    
+    public int getUserIDByUsername(String Username) {
+
+        String query = "SELECT USERID From USERS WHERE USERNAME = '" + Username+"' ";
+        try {
+            Statement stet = con.createStatement();
+            ResultSet rs = stet.executeQuery(query);
+            while (rs.next()) {
+                int UID = rs.getInt("UserID");
+                return UID;
+            }
+        } catch (SQLException e) {
+            System.out.println("SQL Excption " + e);
+        }
+        return -1;
+    }
+
+    public void getUserByID(int ID) {
+
+        String query = "SELECT * From USERS WHERE USERID = " + ID;
+        try {
+            Statement stet = con.createStatement();
+            ResultSet rs = stet.executeQuery(query);
+            //Add date
+            while (rs.next()) {
+                int UID = rs.getInt("UserID");
+                String username = rs.getString("username");
+                String password = rs.getString("password");
+                String firstName = rs.getString("firstName");
+                String lastName = rs.getString("lastName");
+                Date date = rs.getDate("date");
+                System.out.println("User ID: " + UID
+                        + "\nUsername: " + username
+                        + "\nPassword: " + password
+                        + "\nFirst Name: " + firstName
+                        + "\nLast Name: " + lastName
+                        + "\nDate: " + date);
+            }
+        } catch (SQLException e) {
+            System.out.println("SQL Excption " + e);
+        }
+    }
+
+    public boolean SignUp(String Username, String Password, String Firstname, String Lastname) {
+        DBClass.ID++;
+        int randomId = (int) Math.random() * 1000000;
+        LocalDate date = LocalDate.now();
+        String query = "INSERT INTO USERS VALUES (" + randomId + ",'" + Username + "', '" + Password + "', '" + Firstname + "', '" + Lastname + "', '" + date + "')";
+        try {
+            Statement stet = con.createStatement();
+            stet.executeUpdate(query);
+            return true;
+        } catch (SQLException e) {
+            System.out.println("SQL Excption " + e);
+            return false;
+        }
+    }
+
+    public int signin(String username, String password) {
+        String query = "SELECT USERNAME, PASSWORD FROM USERS"
+                + " WHERE USERNAME = '" + username + "' AND PASSWORD = '" + password + "'";
+        try {
+            Statement stet = con.createStatement();
+            ResultSet rs = stet.executeQuery(query);
+            while (rs.next()) {
+                String dusername = rs.getString("username");
+                String dpassword = rs.getString("password");
+                int id = rs.getInt("ID");
+                System.out.println(""
+                        + "\nUsername: " + dusername
+                        + "\nPassword: " + dpassword);
+                return id;
+            }
+        } catch (SQLException e) {
+            System.out.println("SQL Excption " + e);
+        }
+        return -1;
+    }
+
+    public int getID() {
+        return ID;
+    }
+
+    public void getGamessTable() {
+
+        String query = "SELECT * From GAMES";
+
+        try {
+            Statement stet = con.createStatement();
+            ResultSet rs = stet.executeQuery(query);
+            while (rs.next()) {
+                DBClass.RecordID++;
+                int GID = rs.getInt("REcordID");
+                int FirstNumber = rs.getInt("FirstNumber");
+                String SecondNumber = rs.getString("SecondNumber");
+                String Operator = rs.getString("Operator");
+                int GameNumber = rs.getInt("GameNumber");
+                int UID = rs.getInt("UserID");
+                Date date = rs.getDate("date");
+                System.out.println("Game ID: " + GID
+                        + "\nFirst Number: " + FirstNumber
+                        + "\nSecond Number: " + SecondNumber
+                        + "\nOperator: " + Operator
+                        + "\nGameNumber: " + GameNumber
+                        + "\nUser ID: " + UID
+                        + "\nDate: " + date);
+            }
+        } catch (SQLException e) {
+            System.out.println("SQL Excption " + e);
+        }
+    }
+    
+    public void InsertGameRecord(int FirstNumber, int SecondNumber, String Operator,
+            int GameNumber,int UID) {
+        
+        DBClass.RecordID++;
+        LocalDate date = LocalDate.now();
+        String query = "INSERT INTO GAMES VALUES (" + RecordID + "," + FirstNumber + ", " + SecondNumber + ", '" + Operator + "', " + GameNumber + ", " + UID + ", '"+ date + "')";
+
+        try {
+            Statement stet = con.createStatement();
+            stet.executeUpdate(query);
+        } catch (SQLException e) {
+            System.out.println("SQL Excption " + e);
+        }
+    }
+    
+    public int countScoreOfGameByUsername(String username,int GameNumebr){
+
+        String query = "Select Count(*) from GAMES WHERE USERID = "+getUserIDByUsername(username)+" AND GAMENUMBER = "+GameNumebr;
+    
+        try {
+            Statement stet = con.createStatement();
+            ResultSet rs = stet.executeQuery(query);
+            rs.next();
+                int count = rs.getInt(1);
+                return count;
+            
+        } catch (SQLException e) {
+            System.out.println("SQL Excption " + e);
+        }
+        return -1;
+    }
+
+    public int getRecordID() {
+        return RecordID;
+    }
+    
+    
+    
+
+}
